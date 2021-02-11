6744f4971339c1019248c4d81f3c36317e57f6b1
Samson-coder <78577953+Samson-coder@users.noreply.github.com>
2021-02-11 10:57:34 +0530

    Create string.java

---
+++ string.java
@@ -0,0 +1,15 @@
+public class strop{
+    public static void main(String[] args){
+        String s = "Hello";
+        StringBuffer s1 = new StringBuffer("stringbuff ");
+        StringBuilder s2 = new StringBuilder("stringbuild ");
+        System.out.println(s+"Hi");
+        
+        System.out.println(s2.append(s));
+        System.out.println(s1.append(s));
+        System.out.println(s2.reverse());
+        System.out.println(s1.reverse());
+        StringBuffer ss  =new StringBuffer(s);
+        System.out.println(ss.append(s));
+    }
+}
