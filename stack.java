e308648cab12d18570548e9f2851682146f537d9
Samson-coder <78577953+Samson-coder@users.noreply.github.com>
2021-02-16 15:52:24 +0530

    Create stack.java

---
+++ stack.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+public class A{
+    public static void main(String[] args){
+        Stack<Integer> st = new Stack<Integer>();
+        st.push(20);
+        st.push(30);
+        System.out.println(st);
+        st.pop();
+        System.out.println(st);
+        st.push(10);
+        st.push(20);
+        st.push(40);
+        st.push(50);
+        st.push(60);
+        System.out.println(st.pop());
+        System.out.println(st.size());
+        Iterator iterator = st.iterator();
+        While(iterator.hasNext()20){
+            
+        }
+        
+    }
+}
